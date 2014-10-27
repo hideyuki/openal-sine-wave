afa48a04b16c352bb4093b8eba282547d0aaed1e
Hideyuki Takei <takehide22@gmail.com>
2014-10-27 20:55:28 +0900

    Output stereo sound. R is antiphase of L.

--- sine_wave.c
+++ sine_wave.c
@@ -12,7 +12,7 @@
 int main(){
   ALCdevice *device;
   ALCcontext *context;
-  ALshort data[BUFFER_LENGTH];
+  ALshort data[BUFFER_LENGTH*2];
   ALuint buffer, source;
   int i;
 
@@ -22,10 +22,11 @@ int main(){
   alGenBuffers(1, &buffer);
 
   for (i = 0; i < BUFFER_LENGTH; ++i) {
-      data[i] = sin(2 * M_PI * SOUND_HZ * i / BUFFER_LENGTH) * SHRT_MAX;
+      data[i*2] = sin(2 * M_PI * SOUND_HZ * i / BUFFER_LENGTH) * SHRT_MAX;
+      data[i*2+1] = -1 * sin(2 * M_PI * SOUND_HZ * i / BUFFER_LENGTH) * SHRT_MAX;
   }
 
-  alBufferData(buffer, AL_FORMAT_MONO16, data, sizeof(data), BUFFER_LENGTH);
+  alBufferData(buffer, AL_FORMAT_STEREO16, data, sizeof(data), BUFFER_LENGTH*2);
   alGenSources(1, &source);
   alSourcei(source, AL_BUFFER, buffer);
   alSourcei(source, AL_LOOPING, AL_TRUE);
